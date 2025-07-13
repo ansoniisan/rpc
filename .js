18946fad3813b39a01df85371c005697ecd62f39
ansoniisan <anthonymigo25@gmail.com>
2025-07-13 18:20:54 +0900

    Make players choose their RPC selection based on buttons
    The logic of the game (and .js) now asks for players to select buttons, rather than input text, in order to make their choice. The game ends when 5 rounds have been played.

--- .js
+++ .js
@@ -1,19 +1,10 @@
-/* Rock Paper Scissors is a game whereby two players can each
-   choose between three options; rock, paper, or scissors.
-
-   A game is won a player chooses an option that "beats" the other's.
-
-   Rock beats scissors, scissors beats paper, and paper beats rock.
-
-   If both players choose the same option, they have to play again.
-
-   The game can be played for as many rounds as the players want.
-*/
+//PART 1 - DEFINE THE GAME LOGIC
 
 //Make the computer return a random choice between rock, paper, or scissors
 let computerScore = 0;
 let humanScore = 0;
 let ties = 0;
+let gamesPlayed = 0;
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -27,46 +18,102 @@ function getComputerChoice() {
     return "scissors";
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("What's your choice?");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") || (humanChoice == "scissors" && computerChoice == "rock")) {
         computerScore++;
-        return `You lose! ${computerChoice} beats ${humanChoice}`;
     }
     else if ((computerChoice == "rock" && humanChoice == "paper") || (computerChoice == "paper" && humanChoice == "scissors") || (computerChoice == "scissors" && humanChoice == "rock")) {
         humanScore++;
-        return `You win! ${humanChoice} beats ${computerChoice}`;
     }
     else {
         ties++;
-        return "Nobody won that round! Play again!";
     }
+    gamesPlayed++;
+    console.log(gamesPlayed);
 }
 
-// A player only "reigns supreme" if they win more than the opponent 
-// AND have at least as many wins as there are ties.
-function playGame() {
-    for (let i = 0; i < 5; i++) {
-        const humanAnswer = getHumanChoice();
-        const computerAnswer = getComputerChoice();
-        playRound(humanAnswer, computerAnswer)
-    }
 
-    if (computerScore > humanScore && computerScore >= ties) {
-        return "the computer reigns supreme!";
-    }
-    if (computerScore < humanScore && humanScore >= ties) {
-        return "you reign supreme!";
-    }
-    if (computerScore === humanScore && computerScore > ties) {
-        return "you both reign supreme!";
-    }
-    return "none of you reign supreme";
-}
+//PART 2 - MANIPULATE THE DOM
 
-console.log(playGame(), `Final stats: computer score = ${computerScore}, human score = ${humanScore}, ties = ${ties}`);
+let container = document.querySelector("#container");
+container.style.textAlign = "center";
+container.style.gap = 20;
+
+let pageTitle = document.createElement("h1");
+pageTitle.textContent = "Let's play Rock-Paper-Scissors";
+container.appendChild(pageTitle);
+
+let gameImage = document.createElement("img");
+gameImage.src = "https://remptongames.com/wp-content/uploads/2018/02/rock-paper-scissor-ft.jpg";
+gameImage.alt = "a crumpled piece of paper, a pair of scissors, and a rock on a wooden background";
+gameImage.width = 400;
+container.appendChild(gameImage);
+
+let buttonDiv = document.createElement("div");
+container.appendChild(buttonDiv);
+
+let rockButton = document.createElement("button");
+rockButton.textContent = "rock";
+let paperButton = document.createElement("button");
+paperButton.textContent = "paper";
+let scissorsButton = document.createElement("button");
+scissorsButton.textContent = "scissors";
+
+buttonDiv.appendChild(rockButton);
+buttonDiv.appendChild(paperButton);
+buttonDiv.appendChild(scissorsButton);
+
+let resultsDiv = document.createElement("div");
+container.appendChild(resultsDiv);
+
+let scoreboard = document.createElement("h2");
+scoreboard.textContent = "Scoreboard";
+resultsDiv.appendChild(scoreboard);
+
+let rounds = document.createElement("ol");
+resultsDiv.appendChild(rounds);
+
+let buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        let playerSelection = button.textContent;
+        console.log(playerSelection);
+
+        playRound(playerSelection, getComputerChoice());
+
+        let result = document.createElement("li");
+        result.textContent = `Human score: ${humanScore}, computer score: ${computerScore}, ties: ${ties}`;
+        rounds.appendChild(result);
+
+        let results = document.querySelectorAll("li");
+
+        if (gamesPlayed === 5) {
+
+            buttons.forEach(button => button.disabled = true)
+
+            if (computerScore > humanScore) {
+                alert("The computer wins!");
+            }
+            else if (computerScore < humanScore) {
+                alert("You win!");
+            }
+            else { alert("Nobody won!") }
+
+            let startOverButton = document.createElement("button");
+            startOverButton.textContent = "Start over!";
+            resultsDiv.appendChild(startOverButton);
+
+            startOverButton.addEventListener("click", () => {
+                humanScore = 0;
+                computerScore = 0;
+                ties = 0;
+                gamesPlayed = 0;
+                buttons.forEach(button => button.disabled = false)
+                rounds.textContent = "";
+                startOverButton.remove();
+            });
+        }
+
+    });
+});
